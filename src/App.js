cd7c68dfecb292ef8295bd7da6bf6b3a22e1b01d
yasminbraga <yasminbragateixeira@gmail.com>
2020-11-16 16:27:43 -0300

    adiciona tudo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+import './styles.css';
+
+
 
 function App() {
+
+  const [data, setData] = useState([
+    {task: 'Banhar', done: false},
+    {task: 'Comer', done: false},
+    {task: 'Dormir', done: false},
+    {task: 'Jogar', done: false},
+  ]);
+
+  function deletTask(index) {
+    const newState = [...data]
+    newState.splice(index, 1)
+    setData(newState)
+  }
+
+  function doneTask(ev, index) {
+    const newState = [...data]
+    newState[index].done = ev.target.checked
+    setData(newState)
+  }
+
+  function addTask(ev) {
+    if (ev.key === 'Enter') {
+      setData([{task: ev.target.value, done: false}, ...data])
+      ev.target.value = ""
+    }
+  }
+
+  function editTask(ev, index) {
+    const newState = [...data]
+    const value = ev.target.value
+    newState[index].task = value
+
+    setData(newState)
+  }
+
+  function renderItem(data, index) {
+    return (
+      <li key={index.toString()}>
+        <div>
+          <input type="checkbox" onChange={ev => doneTask(ev, index)} defaultChecked={data.done}/>
+          <input 
+            type="text" 
+            onChange={(ev) => editTask(ev, index)}
+            value={data.task} 
+            className="unstyled-input"/>
+        </div>
+  
+        <div className="actions">
+          <button onClick={() => deletTask(index)}>&times;</button>
+        </div>
+      </li>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1 className="title" >ToDo</h1>
+    <div className="todo-container">
+      <input 
+        type="text" 
+        placeholder="Escreva sua tarefa e aperte enter para adicionar" 
+        onKeyDown={addTask}
+      />
+      
+      <ul>{data.map(renderItem)}</ul>
     </div>
+    </>
   );
 }
 
